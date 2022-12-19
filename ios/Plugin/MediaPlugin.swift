2d79ad3d1c60606a018e13be6867de1028543dbb
Salvatore Addesa <salvatore.addesa@gmail.com>
2022-12-19 09:50:00 +0100

    Update MediaPlugin.swift

--- ios/Plugin/MediaPlugin.swift
+++ ios/Plugin/MediaPlugin.swift
@@ -317,15 +317,20 @@ public class MediaPlugin: CAPPlugin {
 
             var a = JSObject()
 
-            self.imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: requestOptions, resultHandler: { (fetchedImage, _) in
+            self.imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFill, options: requestOptions, resultHandler: { (fetchedImage, _) in
                 guard let image = fetchedImage else {
                     return
                 }
 
                 a["identifier"] = asset.localIdentifier
-
+                
                 // TODO: We need to know original type
                 a["data"] = image.jpegData(compressionQuality: CGFloat(thumbnailQuality) / 100.0)?.base64EncodedString()
+                
+                var thumbnailImage = self.resizeImage(image: image, targetSize: thumbnailSize)
+                if thumbnailImage != nil {
+                    a["thumbnail_data"] = thumbnailImage?.jpegData(compressionQuality: CGFloat(thumbnailQuality) / 100.0)?.base64EncodedString()
+                }
 
                 if asset.creationDate != nil {
                     a["creationDate"] = JSDate.toString(asset.creationDate!)
@@ -343,6 +348,32 @@ public class MediaPlugin: CAPPlugin {
         call.resolve([
             "medias": assets
             ])
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
 
 
